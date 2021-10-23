793b71966f4c3ce2a6a89076bc7db59e0d411604
snowinmars <snowinmars@yandex.ru>
2021-10-23 20:48:57 +0400

    Handle exceptions

--- src/circle.py
+++ src/circle.py
@@ -8,7 +8,15 @@ def get_square(radius):
 
 config = dotenv_values('.env')
 string_radius = config['radius']
-radius = int(string_radius)
+
+try:
+    radius = int(string_radius)
+except ValueError:
+    raise ValueError(f'Не могу превратить значение {string_radius} в число')
+
+if radius <= 0:
+    raise ValueError('Радиус круга должен быть больше нуля')
+
 square = get_square(radius)
 
 print(f'Площадь круга радиусом {radius} равна {square}')
